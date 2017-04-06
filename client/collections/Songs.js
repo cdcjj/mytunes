27c7924b9d4acabbc795837efe7a045ba6e9c5d3
cdcjj <chihyundorischiu@gmail.com>
2017-04-06 12:07:00 -0500

    properly connects to Parse

--- client/collections/Songs.js
+++ client/collections/Songs.js
@@ -1,6 +1,25 @@
 // Songs.js - Defines a backbone collection class for songs.
 var Songs = Backbone.Collection.extend({
-
-  model: SongModel
+  url: 'http://parse.atx.hackreactor.com/mytunes/classes/songs',
+  model: SongModel,
+  initialize: function() {
+    this.fetch({
+      reset: true,
+      success: function(response, xhr) {
+        console.info('Inside success');
+      },
+      error: function (errorResponse) {
+        console.log(errorResponse);
+      }
+    });
+  },
+  events: {
+    'add': function () {
+      this.model.trigger('add', this);
+    },
+  },
+  parse: function(response) {
+    return response.results;
+  }
 
 });
--- client/views/LibraryView.js
+++ client/views/LibraryView.js
@@ -5,8 +5,12 @@ var LibraryView = Backbone.View.extend({
 
   initialize: function() {
     this.render();
-
+    this.collection.on('reset', function(model) {
+      this.render();
+    }, this);
   },
+
+
 
   render: function() {
     // to preserve event handlers on child nodes, we must call .detach() on them before overwriting with .html()
